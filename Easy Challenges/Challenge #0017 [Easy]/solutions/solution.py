d99bb79fcbb1501ecfc6e5e448e4e907b09cd7f0
Ahmed Dhanani <ahmed.26.95@outlook.com>
2016-03-20 08:57:20 +0000

    added solution to easy challenge 0016 and 0017

---
+++ Easy Challenges/Challenge #0017 [Easy]/solutions/solution.py
@@ -0,0 +1,33 @@
+import math
+
+def PrintTriangle(height):
+    
+    stars = 1
+    for counter in range(height):
+        starString = ""
+        for starCounter in range(stars):
+            starString += "@"
+        print starString
+        
+        stars *= 2
+        
+def RightJustifiedTriangle(height):
+    
+    stars = 1
+    for counter in range(height):
+        spaces = int(math.pow(2, height) - stars)
+        starString = ""
+        
+        for space in range(spaces):
+            starString += " "
+        
+        for starCounter in range(stars):
+            starString += "@"
+        
+        print starString
+        
+        stars *= 2
+        
+if __name__ == "__main__":
+    PrintTriangle(5)
+    RightJustifiedTriangle(5)
