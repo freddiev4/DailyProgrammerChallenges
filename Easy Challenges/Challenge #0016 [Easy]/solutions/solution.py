b539dd9cdb4ef91a9fd2841277305cd290918322
Ahmed Dhanani <ahmed.26.95@outlook.com>
2016-03-20 08:57:20 +0000

    added solution to easy challenge 0016 and 0017

---
+++ Easy Challenges/Challenge #0016 [Easy]/solutions/solution.py
@@ -0,0 +1,14 @@
+def ManipulateString(stringOne, stringTwo):
+    
+    index = len(stringOne) - 1
+    while index >= 0:
+        if stringOne[index] in stringTwo:
+            stringOne = stringOne[ : index] + stringOne[index + 1 : ]
+            
+        index -= 1
+            
+    return stringOne
+    
+if __name__ == "__main__":
+    text = ManipulateString("Daily Programmer", "aeiou ")
+    print text
