a0e7130f7f5f15a131b7681bf1b497b4234215ac
Andres Villa <afvilla@amazon.com>
2021-10-21 15:45:47 -0400

    solve challenge 1

---
+++ Easy Challenges/[easy] challenge #1/challenge_1_easy.py
@@ -0,0 +1,12 @@
+def main():
+    """Main program
+    """
+    name = input("Enter your name.\n")
+    age = input("Enter your age.\n")
+    reddit_username = input("Enter your reddit username.\n")
+
+    print(f'your name is {name}, you are {age} years old, and your username is {reddit_username}')
+
+
+if __name__ == "__main__":
+    main()
